a47ae8e012862e81aee6e5a3d22ce5f993aa12fe
Your Name <you@example.com>
2018-11-28 21:57:54 +0800

    aa

--- main.go
+++ main.go
@@ -59,7 +59,13 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	// r := setupRouter()
+	// r.Run(":8080")
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context){
+		c.String(http.StatusOK, "pong")
+	})
+
+	router.Run(":8080")
 }
